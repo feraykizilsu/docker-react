2e7c23ef14c07e781a7d9ab86a7daa7e0f3d8fbf
FerayKizilsu <semihkizilsu@Semihs-MacBook-Pro.local>
2019-05-14 10:52:02 +0300

    dfsfsf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I was changes! 
+         I was changes sdfsdf! 
         </p>
         <a
           className="App-link"
